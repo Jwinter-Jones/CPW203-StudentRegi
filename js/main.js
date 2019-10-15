1956800d5156dc4c44f97f2341f25a605b62676e
JAMES Winter-jones <winter-jones8308@students.cptc.edu>
2019-10-15 14:31:18 -0700

    working student reg and clearform

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ var stu = new Student();
 stu.firstName = "Joe"; // SET
 var fName = stu.firstName; // GET
 stu.address = "123 Fake street";
+//Constants
+var programAttr = "data-program";
 window.onload = main;
 function main() {
     var regBtn = document.querySelector("button");
@@ -20,6 +22,21 @@ function processForm() {
     var nextStu = getStudentFromForm();
     displayStudent(nextStu);
     clearForm();
+}
+/**
+ * Clears all the inputs in the form
+ */
+function clearForm() {
+    var regForm = document.getElementById("reg-form");
+    regForm.reset();
+    /*
+    let allTextBoxes = document.querySelectorAll("input[type=text]");
+
+    for(let i = 0; i < allTextBoxes.length; i++){
+        let currBox = <HTMLInputElement> allTextBoxes[i];
+        currBox.value = "";
+    }
+    */
 }
 function getStudentFromForm() {
     var tempStu = new Student();
@@ -41,4 +58,29 @@ function displayStudent(s) {
     newItem.innerText = s.firstName + " " + s.lastName;
     var displaySection = document.querySelector("#student-list>ul");
     displaySection.appendChild(newItem);
+    var list = displaySection.querySelector("ul");
+    //Embed student data in <li>
+    newItem.setAttribute("data-program", s.program);
+    newItem.setAttribute("data-address", s.address);
+    newItem.setAttribute("data-birthdate", s.dateOfBirth.toString());
+    console.log(newItem);
+    // Call showStudentData when the <li> is clicked
+    newItem.onclick = showStudentData;
+    // Add <li> as a child to <ul>
+    list.appendChild(newItem);
+}
+/**
+ * Showas individual student data when
+ * a student <li> is clicked
+ */
+function showStudentData() {
+    //console.log(this);
+    var currListItem = this;
+    var name = currListItem.innerText;
+    var program = currListItem.getAttribute(programAttr);
+    // alert(name + " is studyinng " + program);
+    var h2 = document.querySelector("#display h2");
+    h2.innerHTML = name;
+    var p = document.querySelector("#display > p");
+    p.innerHTML = name + " is studying " + program;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -31,6 +31,10 @@ stu.firstName = "Joe"; // SET
 let fName = stu.firstName; // GET
 stu.address = "123 Fake street";
 
+//Constants
+const programAttr = "data-program";
+
+
 window.onload = main;
 
 function main(){
@@ -42,6 +46,22 @@ function processForm(){
     let nextStu:Student = getStudentFromForm();
     displayStudent(nextStu);
     clearForm();
+}
+
+/**
+ * Clears all the inputs in the form
+ */
+function clearForm(){
+    let regForm = <HTMLFormElement> document.getElementById("reg-form");
+    regForm.reset();
+    /*
+    let allTextBoxes = document.querySelectorAll("input[type=text]");
+
+    for(let i = 0; i < allTextBoxes.length; i++){
+        let currBox = <HTMLInputElement> allTextBoxes[i];   
+        currBox.value = "";     
+    }
+    */
 }
 
 function getStudentFromForm():Student{
@@ -66,4 +86,36 @@ function displayStudent(s:Student):void{
     newItem.innerText = s.firstName + " " + s.lastName;
     let displaySection = document.querySelector("#student-list>ul");
     displaySection.appendChild(newItem);
+    let list = displaySection.querySelector("ul");
+
+    //Embed student data in <li>
+    newItem.setAttribute("data-program", s.program);
+    newItem.setAttribute("data-address", s.address);
+    newItem.setAttribute("data-birthdate", s.dateOfBirth.toString());
+
+    console.log(newItem);
+
+    // Call showStudentData when the <li> is clicked
+    newItem.onclick = showStudentData;
+    
+    // Add <li> as a child to <ul>
+    list.appendChild(newItem);
+
+}
+
+/**
+ * Showas individual student data when
+ * a student <li> is clicked
+ */
+function showStudentData(){
+    //console.log(this);
+    let currListItem = <HTMLLIElement> this;
+    let name = currListItem.innerText;
+    let program = currListItem.getAttribute(programAttr);
+    // alert(name + " is studyinng " + program);
+    let h2 = document.querySelector("#display h2");
+    h2.innerHTML = name;
+
+    let p = document.querySelector("#display > p");
+    p.innerHTML = name + " is studying " + program;
 }
